c62415e4e4fec7b3930f8eaa9d2632c6bc3d2eb0
eric <hyongzhe@mails.ccsf.edu>
2017-07-04 19:08:57 -0700

    initialize

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def Home
+  end
+
+  def Help
+  end
+
+  def About
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get '/home',to: 'static_pages#Home'
+
+  get '/help', to:'static_pages#Help'
+
+  get '/about', to:'static_pages#About'
+
+  get '/signup', to:'users#new'
+  
+  root 'static_pages#Home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
